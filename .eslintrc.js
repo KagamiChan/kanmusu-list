b9105b0c437e41cbb8d901d36465c2e9a5432d58
Kagami <moe.pascal@gmail.com>
2021-04-03 18:33:43 +0800

    feat: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["import", "react", "prettier", "@typescript-eslint"],
+  extends: [
+    "airbnb",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript",
+    "prettier",
+  ],
+  rules: {
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": "off",
+    "no-use-before-define": "off",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        ts: "never",
+        tsx: "never",
+        js: "never",
+        jsx: "never",
+      },
+    ],
+  },
+};
